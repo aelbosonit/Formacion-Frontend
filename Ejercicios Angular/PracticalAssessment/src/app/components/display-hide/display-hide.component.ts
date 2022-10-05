7796de08a7246bcb8dd9fe2306c3a566890b0b75
aelbosonit <andres.enrique@bosonit.com>
2022-10-05 10:01:51 +0200

    Adding Display-Hide

--- Ejercicios Angular/PracticalAssessment/src/app/components/display-hide/display-hide.component.ts
+++ Ejercicios Angular/PracticalAssessment/src/app/components/display-hide/display-hide.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-display-hide',
   templateUrl: './display-hide.component.html',
   styleUrls: ['./display-hide.component.css']
 })
-export class DisplayHideComponent implements OnInit {
+export class DisplayHideComponent{
 
-  constructor() { }
+  show: boolean = false; // Para saber si vamos a mostrar o no
+  value: string = 'Enseñar'; // Valor de texto del botón
 
-  ngOnInit(): void {
+  /**
+   * Cambiar el valor de show cada vez que se pulsa.
+   * Comprueba si el texto del botón es el correcto.
+   */
+  displayAndHide() {
+    this.show = !this.show; // Valor opuesto
+    if(this.value === 'Enseñar') { // Cuando se pulse
+      this.value = 'Esconder'; // El valor va a cambiar
+    } else if(this.value ==='Esconder') { // Cuando se vuelva a pulsar
+      this.value = 'Enseñar'; // Lo mismo
+    }
   }
-
 }
